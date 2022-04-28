50e7dabeee9b9ccccdf81ebec631f2c33ab3a7e9
unknown <you@example.com>
2022-04-28 06:35:00 +0530

    Added: Ability to create Contacts in AddressBook

--- src/com/bl/javastreams/Main.java
+++ src/com/bl/javastreams/Main.java
@@ -2,6 +2,16 @@ package com.bl.javastreams;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome To The Java Streams Problems");
+
+        PersonDetails person = new PersonDetails();
+        person.firstName("Salman");
+        person.lastName("Pasha");
+        person.Address("BTM 1st Stage");
+        person.City("Bangalore");
+        person.State("Karnataka");
+        person.ZipCode(560029);
+        person.PhoneNumber("9845715264");
+        person.Email("mks@gmail.com");
+        System.out.println(person.toString());
     }
 }
