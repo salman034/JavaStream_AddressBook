5da38e1e3551d6dcf07b31536a3b951aa34d60f2
unknown <you@example.com>
2022-04-30 06:40:16 +0530

    Added: Ability to ensure there is no Duplicate Entry of the same person

--- src/com/bl/javastreams/PersonDetails.java
+++ src/com/bl/javastreams/PersonDetails.java
@@ -10,8 +10,7 @@ public class PersonDetails {
     private String phoneNumber;
     private String email;
 
-    public PersonDetails(String firstName, String lastName, String address, String city, String state,
-                         String zipCode, String phoneNumber,  String email) {
+    public PersonDetails(String firstName, String lastName, String address, String city, String state, String zipCode, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -30,56 +29,28 @@ public class PersonDetails {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
     }
 
     public void setAddress(String address) {
         this.address = address;
     }
 
-    public String getCity() {
-        return city;
-    }
-
     public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getState() {
-        return state;
     }
 
     public void setState(String state) {
         this.state = state;
     }
 
-    public String getZipCode() {
-        return zipCode;
-    }
-
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
